896a757bc362c8f26b6fd071bb5727720a2a32f4
nuengtidakongsat <naungtida.kongsat@e-tech.ac.th>
2026-05-29 13:47:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/lab1_20/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab1_20
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/test.dart
@@ -0,0 +1,11 @@
+
+void main(){
+
+  String fname = "Nuengtida";
+  int age = 18;
+  double money = 1.56;
+  bool x = true;
+
+  print("Hello,$fname $age $money $x");
+
+}
